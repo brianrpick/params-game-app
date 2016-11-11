4ab1ead90b485a196e3a290e8562d3f156f7f0b3
Brian Pick <brianr.pick@gmail.com>
2016-11-10 19:14:14 -0800

    finished number game, moved code into controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,18 @@ class GamesController < ApplicationController
     @first_name = params[:first_name]
     @last_name = params[:last_name]
     @input_answer = params[:input_answer]
-
   end
 
+  def number_game
+    guess = params[:guess]
+    @answer = 36
+    @check = " "
+    if guess.to_i == 36
+      @check = "You Won!"
+    elsif guess.to_i < 36
+      @check = "Guess higher!"
+    elsif guess.to_i > 36
+      @check = "Guess lower!"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/name", to: "games#home"
+  get "/number_game", to: "games#number_game"
 end
