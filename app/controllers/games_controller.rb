0500c35fb6bf3f679a098610995b153194edc184
Brian Pick <brianr.pick@gmail.com>
2016-11-10 19:53:25 -0800

    Added url_param number game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,4 +17,19 @@ class GamesController < ApplicationController
       @check = "Guess lower!"
     end
   end
+
+  def url_number_game
+    guess = params[:guess].to_i
+    answer = 36
+    @check = " "
+
+    if guess == answer
+      @check = "You Won!"
+    elsif guess < answer
+      @check = "Guess higher!"
+    elsif guess > answer
+      @check = "Guess lower!"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/name", to: "games#home"
   get "/number_game", to: "games#number_game"
+  get "/url_number_game/:guess", to: "games#url_number_game"
+
 end
